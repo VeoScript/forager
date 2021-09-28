1e354a58672f04f78f05652832f6ee3d6f939966
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-09-28 17:20:35 +0800

    feat: static data

---
+++ src/mock/index.ts
@@ -0,0 +1,117 @@
+export const trending = [
+  {
+    name: 'Onion',
+    recipes: '100 Possible recipes'
+  },
+  {
+    name: 'Muriatic Acid',
+    recipes: '1M Possible recipes'
+  },
+  {
+    name: 'Cyanide Powder',
+    recipes: '500M Possible recipes'
+  },
+  {
+    name: 'Sugar',
+    recipes: '10k Possible recipes'
+  },
+  {
+    name: 'Dried Bay Leaves',
+    recipes: '880 Possible recipes'
+  }
+]
+
+export const newsfeed = [
+  {
+    avatar: 'https://avatars.githubusercontent.com/u/26340308?v=4',
+    name: 'Lisa Manoban',
+    username: 'lalalalisa_m',
+    food: 'Filipino Adobo',
+    tag: 'Philippine Food',
+    ingredients: [
+      {
+        name: 'Onion'
+      },
+      {
+        name: 'Dried Bay Leaves'
+      },
+      {
+        name: 'Whole Peppercorn'
+      },
+      {
+        name: 'Sugar'
+      }
+    ],
+    reaction: '43',
+    bookmarked: '115',
+  },
+  {
+    avatar: 'https://upload.wikimedia.org/wikipedia/commons/5/5f/Jennie_Kim_from_BLACKPINK_PUBG_210321_%28cropped%29.jpg',
+    name: 'Jennie Kim',
+    username: 'jennierubyjane',
+    food: 'Kimchi',
+    tag: 'Korean Food',
+    ingredients: [
+      {
+        name: 'Taeng Hinog'
+      },
+      {
+        name: 'Panis Na Laway'
+      },
+      {
+        name: 'Katas Ng Medyas'
+      },
+      {
+        name: 'Buhok Sa Ilok'
+      }
+    ],
+    reaction: '1k',
+    bookmarked: '50k',
+  },
+  {
+    avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKJzUVFy_ecmb3RjxXgLZ-JsxIAOd4tldbrA&usqp=CAU',
+    name: 'Roseanne Park',
+    username: 'roses_are_rosie',
+    food: 'Pizza',
+    tag: 'American Food',
+    ingredients: [
+      {
+        name: 'Pan-os Na Pan'
+      },
+      {
+        name: 'Uk-ok'
+      },
+      {
+        name: 'Double Dead Meat'
+      },
+      {
+        name: 'PASAS'
+      },
+      {
+        name: 'PINEAPPLE'
+      }
+    ],
+    reaction: '100k',
+    bookmarked: '809',
+  },
+  {
+    avatar: 'https://pbs.twimg.com/media/EzH3-JTXIAAH6H9.jpg',
+    name: 'Kim Jisoo',
+    username: 'sooya',
+    food: 'Sinigang Na Cyanide',
+    tag: 'Toxic People Food',
+    ingredients: [
+      {
+        name: 'Muriatic Acid'
+      },
+      {
+        name: 'Cyanide Powder'
+      },
+      {
+        name: 'Ammonium Sulfide'
+      }
+    ],
+    reaction: '1M',
+    bookmarked: '5M',
+  }
+]
