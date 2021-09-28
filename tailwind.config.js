2dde33a8b7a26354686d3248c4718305d8768dc7
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-09-28 17:16:18 +0800

    style: add default colors in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,14 @@ module.exports = {
   darkMode: false,
   theme: {
     extend: {
+      colors: {
+        'pure-white': '#FFFFFF',
+        'ghost-white': '#FAFAFA',
+        'black-matt': '#1E1E1E',
+        'black-dim': '#393939',
+        'dark-gray': '#4F4F4F',
+        'light-gray': '#7F7F7F',
+      },
       fontFamily: {
         poppins: ['Poppins', ...defaultTheme.fontFamily.serif],
       }
