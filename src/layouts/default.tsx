1b6bb5ebe217a4160ecf2cad64b562e72e6e5fe3
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-09-29 10:05:59 +0800

    style: add responsive design for default layout

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import Navbar from '~/components/Navbar'
+import NavBar from '~/components/MenuBars/NavBar'
+import BottomBar from '~/components/MenuBars/BottomBar'
 
 interface TypeProps {
   children: any
@@ -10,10 +11,11 @@ const Layout: React.FC<TypeProps> = ({ children }) => {
     <div className="font-poppins flex flex-col items-center w-full max-w-full h-screen overflow-y-auto cursor-default text-black-dim bg-ghost-white">
       <div className="flex flex-col items-center w-full h-full">
         <div className="relative flex w-full">
-          <Navbar />
+          <NavBar />
+          <BottomBar />
         </div>
-        <div className="flex flex-col items-center w-full max-w-5xl h-screen py-16">
-          <div className="flex w-full pb-5">
+        <div className="flex flex-col items-center w-full max-w-5xl h-screen py-14 md:py-16">
+          <div className="flex w-full max-w-full pb-10 md:pb-5">
             {children}
           </div>
         </div>
