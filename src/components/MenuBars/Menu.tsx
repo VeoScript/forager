d3f95da17111156f89c626e72f73f6cb9037e598
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-09-29 12:07:32 +0800

    feat: add menu toggle and its content

---
+++ src/components/MenuBars/Menu.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import MenuLinks from './Options/MenuLinks'
+import AccountLinks from './Options/AccountLinks'
+import Credits from './Options/Credits'
+import { RiCloseLine } from 'react-icons/ri'
+
+interface TypeProps {
+  isMenuOpen: any
+  setMenuOpen: any
+}
+
+const Menu: React.FC<TypeProps> = ({ isMenuOpen, setMenuOpen }) => {
+  return (
+    <React.Fragment>
+      {isMenuOpen && (
+        <React.Fragment>
+          <button onClick={() => {setMenuOpen(false)}} type="button" className={`${isMenuOpen ? 'z-10 block fixed inset-0 w-full h-full cursor-default focus:outline-none' : 'hidden'}`} />
+          <div className="fixed md:hidden inset-0 z-50 flex flex-col w-full h-full overflow-y-auto bg-ghost-white">
+            <div className="flex items-center justify-center w-full p-3 border-b border-black-matt border-opacity-10">
+              <button
+                className="absolute left-5"
+                type="button"
+                onClick={() => {
+                  setMenuOpen(false)
+                }}
+              >
+                <RiCloseLine className="w-6 h-6" />
+              </button>
+              <div className="flex justify-center w-full">
+                <h3 className="font-normal text-base">Options</h3>
+              </div>
+            </div>
+            <div className="flex flex-col w-full">
+              <MenuLinks />
+              <AccountLinks />
+              <Credits />
+            </div>
+          </div>
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  )
+}
+
+export default Menu
---
+++ src/components/MenuBars/Options/MenuLinks.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Link from 'next/link'
+import { RiArrowRightSLine } from 'react-icons/ri'
+
+const MenuLinks: React.FC = () => {
+  return (
+    <React.Fragment>
+      <div className="flex w-full px-5 py-3 bg-light-gray bg-opacity-5 border-b border-black-matt border-opacity-10">
+        <h3 className="font-normal text-sm uppercase">Menu</h3>
+      </div>
+      <div className="flex flex-col w-full">
+        <Link href="/">
+          <a className="flex flex-row items-center justify-between w-full px-5 py-3 border-b border-black-matt border-opacity-10 hover:border-transparent hover:bg-light-gray hover:bg-opacity-10">
+            <h1 className="text-sm">Home</h1>
+            <RiArrowRightSLine className="w-6 h-6 text-light-gray" />
+          </a>
+        </Link>
+        <Link href="/">
+          <a className="flex flex-row items-center justify-between w-full px-5 py-3 border-b border-black-matt border-opacity-10 hover:border-transparent hover:bg-light-gray hover:bg-opacity-10">
+            <span className="text-sm">Discover</span>
+            <RiArrowRightSLine className="w-6 h-6 text-light-gray" />
+          </a>
+        </Link>
+        <Link href="/">
+          <a className="flex flex-row items-center justify-between w-full px-5 py-3 border-b border-black-matt border-opacity-10 hover:border-transparent hover:bg-light-gray hover:bg-opacity-10">
+            <span className="text-sm">Activity</span>
+            <RiArrowRightSLine className="w-6 h-6 text-light-gray" />
+          </a>
+        </Link>
+        <Link href="/">
+          <a className="flex flex-row items-center justify-between w-full px-5 py-3 border-b border-black-matt border-opacity-10 hover:border-transparent hover:bg-light-gray hover:bg-opacity-10">
+            <span className="text-sm">Bookmarks</span>
+            <RiArrowRightSLine className="w-6 h-6 text-light-gray" />
+          </a>
+        </Link>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default MenuLinks
