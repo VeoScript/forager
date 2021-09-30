5b40777d498a8eafdda61145308fce6fa0a34c12
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-09-30 13:39:25 +0800

    feat: add post description display (#20)

--- src/components/Home/NewsFeed.tsx
+++ src/components/Home/NewsFeed.tsx
@@ -28,6 +28,9 @@ const NewsFeed: React.FC = () => {
               </div>
             </div>
           </div>
+          <div className="flex flex-col w-full px-3 py-2 border-b border-black-matt border-opacity-10">
+            <span className="font-normal text-xs">{ feed.description }</span>
+          </div>
           <div className="flex flex-col w-full mt-2 space-y-2">
             <div className="flex flex-row items-center justify-between w-full px-3">
               <h3 className="font-semibold text-sm text-dark-gray text-opacity-80">Main Ingredients</h3>
