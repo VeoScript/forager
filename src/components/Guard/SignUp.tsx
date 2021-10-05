650f82b584b479df89efd74f881c585b69b4b05d
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-10-05 18:18:42 +0800

    refactor: create form components for signin and signup page

---
+++ src/components/Guard/SignUp.tsx
@@ -0,0 +1,130 @@
+import React from 'react'
+import Link from 'next/link'
+import Spinner from '~/utils/Spinner'
+import { motion } from 'framer-motion'
+import { useForm } from 'react-hook-form'
+
+interface FormData {
+  email: string
+  password: string
+}
+
+const SignUpComponent: React.FC = () => {
+
+  const { register, handleSubmit, reset, formState: { isSubmitting } } = useForm()
+
+  async function onSignUp(formData: FormData) {
+    console.log(formData)
+    reset()
+  }
+
+  return (
+    <div className="relative flex flex-col items-center w-full h-auto">
+      <motion.div
+        initial={{ scale: 1 }}
+        animate={{ scale: 0 }}
+        transition={{ delay: 1 }}
+        className="absolute inset-0 flex flex-col items-center justify-center h-full"
+      >
+        <div className="flex flex-col items-center w-full h-auto">
+          <h3 className="font-bold text-4xl">FORAGER</h3>
+          <h5 className="font-light text-xl">음식이나 식량을 널리 구하는 사람</h5>
+          <Spinner />
+        </div>
+      </motion.div>
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{ delay: 1 }}
+        className="flex flex-col items-center w-full space-y-5"
+      >
+        <div className="flex flex-col items-center w-full h-auto">
+          <h3 className="font-bold text-2xl">FORAGER</h3>
+          <h5 className="font-light text-base">음식이나 식량을 널리 구하는 사람</h5>
+          <div className="flex flex-col md:flex-row items-center justify-center w-full space-x-1 mt-3">
+            <span className="font-semibold text-sm">CREATE ACCOUNT</span>
+          </div>
+        </div>
+        <form onSubmit={handleSubmit(onSignUp)} className="flex flex-col items-center w-full space-y-2">
+          <div className="flex flex-col w-full px-3 py-2 space-y-1 bg-pure-white border border-black-matt border-opacity-10 focus-within:border-dark-gray">
+            <div className="flex items-center justify-between w-full">
+              <span className="text-xs text-light-gray text-opacity-80">Name</span>
+            </div>
+            <input
+              className="w-full font-normal text-base outline-none bg-transparent"
+              type="text"
+              {...register("name", { required: true })}
+            />
+          </div>
+          <div className="flex flex-col md:flex-row items-center w-full space-x-0 space-y-2 md:space-x-2 md:space-y-0">
+            <div className="flex flex-col w-full px-3 py-2 space-y-1 bg-pure-white border border-black-matt border-opacity-10 focus-within:border-dark-gray">
+              <div className="flex items-center justify-between w-full">
+                <span className="text-xs text-light-gray text-opacity-80">Phone</span>
+              </div>
+              <input
+                className="w-full font-normal text-base outline-none bg-transparent"
+                type="text"
+                {...register("phone", { required: true })}
+              />
+            </div>
+            <div className="flex flex-col w-full px-3 py-2 space-y-1 bg-pure-white border border-black-matt border-opacity-10 focus-within:border-dark-gray">
+              <div className="flex items-center justify-between w-full">
+                <span className="text-xs text-light-gray text-opacity-80">Username</span>
+              </div>
+              <input
+                className="w-full font-normal text-base outline-none bg-transparent"
+                type="text"
+                {...register("username", { required: true })}
+              />
+            </div>
+          </div>
+          <div className="flex flex-col w-full px-3 py-2 space-y-1 bg-pure-white border border-black-matt border-opacity-10 focus-within:border-dark-gray">
+            <div className="flex items-center justify-between w-full">
+              <span className="text-xs text-light-gray text-opacity-80">Email</span>
+            </div>
+            <input
+              className="w-full font-normal text-base outline-none bg-transparent"
+              type="text"
+              {...register("email", { required: true })}
+            />
+          </div>
+          <div className="flex flex-col md:flex-row items-center w-full space-x-0 space-y-2 md:space-x-2 md:space-y-0">
+            <div className="flex flex-col w-full px-3 py-2 space-y-1 bg-pure-white border border-black-matt border-opacity-10 focus-within:border-dark-gray">
+              <div className="flex items-center justify-between w-full">
+                <span className="text-xs text-light-gray text-opacity-80">Create Password</span>
+              </div>
+              <input
+                className="w-full font-normal text-base outline-none bg-transparent"
+                type="password"
+                {...register("password", { required: true })}
+              />
+            </div>
+            <div className="flex flex-col w-full px-3 py-2 space-y-1 bg-pure-white border border-black-matt border-opacity-10 focus-within:border-dark-gray">
+              <div className="flex items-center justify-between w-full">
+                <span className="text-xs text-light-gray text-opacity-80">Re-type Password</span>
+              </div>
+              <input
+                className="w-full font-normal text-base outline-none bg-transparent"
+                type="password"
+                {...register("repassword", { required: true })}
+              />
+            </div>
+          </div>
+          <button
+            className="flex justify-center w-full p-4 text-sm text-pure-white border border-black-matt border-opacity-10 bg-black-matt hover:bg-opacity-90 transition ease-in-out duration-200 outline-none"
+            type="submit"
+          >
+            Sign Up
+          </button>
+          <div className="flex items-center justify-center w-full">
+            <Link href="/signin">
+              <a className="py-2 font-normal text-xs text-black-matt hover:underline">Go back to Sign in</a>  
+            </Link>
+          </div>
+        </form>
+      </motion.div>
+    </div>
+  )
+}
+
+export default SignUpComponent
--- src/layouts/guard.tsx
+++ src/layouts/guard.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Credits from '~/components/Home/Credits'
 
 interface TypeProps {
   children: any
@@ -8,13 +7,13 @@ interface TypeProps {
 const Guard: React.FC<TypeProps> = ({ children }) => {  
   return (
     <div className="font-poppins flex flex-col items-center w-full max-w-full h-screen overflow-hidden cursor-default text-black-dim bg-ghost-white">
-      <div className="flex flex-col items-center justify-center w-full max-w-5xl h-full px-5">
+      <div className="flex flex-col items-center justify-start md:justify-center w-full max-w-5xl h-full p-5 md:p-0">
         <div className="flex justify-center w-full">
           {children}
         </div>
-        <div className="fixed bottom-0 p-3">
-          <span className="font-normal text-xs">© { new Date().getFullYear() } FORAGER FROM VOIDEV</span>
-        </div>
+      </div>
+      <div className="absolute bottom-0 py-5">
+        <span className="font-normal text-xs">© { new Date().getFullYear() } FORAGER FROM VOIDEV</span>
       </div>
     </div>
   )
