be81a4fe36540cf7c92c78a027945a01e8ff2933
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-10-05 20:56:44 +0800

    style: change animation design

--- src/components/Guard/SignUp.tsx
+++ src/components/Guard/SignUp.tsx
@@ -11,7 +11,7 @@ interface FormData {
 
 const SignUpComponent: React.FC = () => {
 
-  const { register, handleSubmit, reset, formState: { isSubmitting } } = useForm()
+  const { register, handleSubmit, reset, formState: { errors, isSubmitting } } = useForm()
 
   async function onSignUp(formData: FormData) {
     console.log(formData)
@@ -23,7 +23,7 @@ const SignUpComponent: React.FC = () => {
       <motion.div
         initial={{ scale: 1 }}
         animate={{ scale: 0 }}
-        transition={{ delay: 1 }}
+        transition={{ delay: 1.5 }}
         className="absolute inset-0 flex flex-col items-center justify-center h-full"
       >
         <div className="flex flex-col items-center w-full h-auto">
@@ -35,7 +35,7 @@ const SignUpComponent: React.FC = () => {
       <motion.div
         initial={{ scale: 0 }}
         animate={{ scale: 1 }}
-        transition={{ delay: 1 }}
+        transition={{ delay: 1.5 }}
         className="flex flex-col items-center w-full space-y-5"
       >
         <div className="flex flex-col items-center w-full h-auto">
@@ -64,7 +64,7 @@ const SignUpComponent: React.FC = () => {
               <input
                 className="w-full font-normal text-base outline-none bg-transparent"
                 type="text"
-                {...register("phone", { required: true })}
+                {...register("phone", { required: true, pattern: /\d+/ })}
               />
             </div>
             <div className="flex flex-col w-full px-3 py-2 space-y-1 bg-pure-white border border-black-matt border-opacity-10 focus-within:border-dark-gray">
@@ -85,7 +85,7 @@ const SignUpComponent: React.FC = () => {
             <input
               className="w-full font-normal text-base outline-none bg-transparent"
               type="text"
-              {...register("email", { required: true })}
+              {...register("email", { required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i })}
             />
           </div>
           <div className="flex flex-col md:flex-row items-center w-full space-x-0 space-y-2 md:space-x-2 md:space-y-0">
@@ -96,7 +96,7 @@ const SignUpComponent: React.FC = () => {
               <input
                 className="w-full font-normal text-base outline-none bg-transparent"
                 type="password"
-                {...register("password", { required: true })}
+                {...register("password", { required: true, pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&^_-]{8,}$/ })}
               />
             </div>
             <div className="flex flex-col w-full px-3 py-2 space-y-1 bg-pure-white border border-black-matt border-opacity-10 focus-within:border-dark-gray">
@@ -110,6 +110,17 @@ const SignUpComponent: React.FC = () => {
               />
             </div>
           </div>
+          <div className="flex w-full px-5 text-xs">
+            {errors.password && 
+              <ul className="text-green-500 list-disc">
+                <li>At least 1 Alphabet</li>
+                <li>At least 1 Digit</li>
+                <li>Contains no space</li>
+                <li>Optional special characters e.g. @$!%*#?&^_-</li>
+                <li>Minimum 8 characters long</li>
+              </ul>
+            }
+          </div>
           <button
             className="flex justify-center w-full p-4 text-sm text-pure-white border border-black-matt border-opacity-10 bg-black-matt hover:bg-opacity-90 transition ease-in-out duration-200 outline-none"
             type="submit"
@@ -118,7 +129,7 @@ const SignUpComponent: React.FC = () => {
           </button>
           <div className="flex items-center justify-center w-full">
             <Link href="/signin">
-              <a className="py-2 font-normal text-xs text-black-matt hover:underline">Go back to Sign in</a>  
+              <a className="py-1 font-normal text-xs text-black-matt hover:underline">Go back to Sign in</a>  
             </Link>
           </div>
         </form>
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import { motion } from 'framer-motion'
 import React from 'react'
 import Head from 'next/head'
 import Guard from '~/layouts/guard'
@@ -11,9 +12,14 @@ const SignUp: NextPage = () => {
         <title>Create Account | Forager</title>
       </Head>
       <Guard>
-        <div className="flex flex-col items-center w-full max-w-xl">
+        <motion.div
+          initial={{ y: -500 }}
+          animate={{ y: 0 }}
+          transition={{ ease: "easeOut", duration: 1 }}
+          className="flex flex-col items-center w-full max-w-xl"
+        >
           <SignUpComponent />
-        </div>
+        </motion.div>
       </Guard>
     </React.Fragment>
   )
