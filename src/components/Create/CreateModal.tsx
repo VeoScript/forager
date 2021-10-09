ee7f9cb2590647f5994798d4b73661d9d99173e3
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-10-09 13:54:36 +0800

    feat: add create dish post functionality and api route (#40)

--- src/components/Create/CreateModal.tsx
+++ src/components/Create/CreateModal.tsx
@@ -3,7 +3,11 @@ import PostCard from './PostCard'
 import { Dialog, Transition } from '@headlessui/react'
 import { RiAddBoxLine } from 'react-icons/ri'
 
-const CreateModal: React.FC = () => {
+interface TypeProps {
+  host: any
+}
+
+const CreateModal: React.FC<TypeProps> = ({ host }) => {
   let [isOpen, setIsOpen] = React.useState(false)
 
   function closeModal() {
@@ -69,7 +73,7 @@ const CreateModal: React.FC = () => {
             >
               <div className="inline-block w-full max-w-5xl py-10 overflow-hidden text-left align-middle transition-all transform">
                 <div className="flex w-full bg-white shadow-xl">
-                  <PostCard setIsOpen={setIsOpen} />
+                  <PostCard host={host} setIsOpen={setIsOpen} />
                 </div>
               </div>
             </Transition.Child>
