1fca5454e9119ce441fac6573fa3d7b023071f9f
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-10-09 15:08:56 +0800

    chore: change dishTitle to dishId in ingredients model (#42)

--- src/pages/api/dishes/index.ts
+++ src/pages/api/dishes/index.ts
@@ -2,17 +2,6 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '~/lib/Prisma'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const get_ingredient = await prisma.dishes.findMany({
-    select: {
-      title: true,
-      category: true,
-      description: true,
-      ingredients: {
-        select: {
-          ingredient: true
-        }
-      }
-    }
-  })
-  res.status(200).json(get_ingredient)
+  const get_dishes = await prisma.dishes.findMany()
+  res.status(200).json(get_dishes)
 }
