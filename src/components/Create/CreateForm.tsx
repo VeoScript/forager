e1a61e53dde13bcfd7122c289df7801d89302f39
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-10-10 15:34:31 +0800

    refactor: udpate create post card layout and its function

--- src/components/Create/CreateForm.tsx
+++ src/components/Create/CreateForm.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import PublishButton from './PublishButton'
+import AutoScroll from '@brianmcallister/react-auto-scroll'
+import IngredientsIcon from '~/utils/Icons/Ingredients'
 import { useForm, useFieldArray } from 'react-hook-form'
 import { RiAddLine, RiDeleteBin4Line, RiCloseFill } from 'react-icons/ri'
 
@@ -55,7 +57,7 @@ const PostCard: React.FC<TypeProps> = ({ host, setIsOpen }) => {
       {/* display create post form controls list */}
       <div className="flex flex-col w-full h-full space-y-5">
         <div className="flex items-center justify-between w-full">
-          <h3 className="font-semibold text-lg">Create Post</h3>
+          <h3 className="font-semibold text-base md:text-lg">Create Post</h3>
         </div>
         <div className="flex flex-col w-full space-y-2">
           <div className="flex flex-col items-center w-full space-y-2">
@@ -91,7 +93,7 @@ const PostCard: React.FC<TypeProps> = ({ host, setIsOpen }) => {
             />
           </div>
           <button
-            className="fixed top-12 right-3 md:hidden transform hover:scale-95 transition ease-in-out duration-200 outline-none"
+            className="fixed top-3 right-3 md:hidden transform hover:scale-95 transition ease-in-out duration-200 outline-none"
             type="button"
             onClick={() => {
               setIsOpen(false)
@@ -108,18 +110,27 @@ const PostCard: React.FC<TypeProps> = ({ host, setIsOpen }) => {
           >
             Cancel
           </button>
-          <div className="flex w-full">
+          <div className="hidden md:flex w-full">
             <h3 className="font-light text-xs text-light-gray">
               <span className="font-bold">Note: </span>
               <span>{`Don't hit enter if your're not already done inputting your ingredients, because it will published automatically.`}</span>
             </h3>
+          </div>
+          <div className="hidden md:flex w-full">
+            {fields.length !== 0 && (
+              <div className="flex w-full">
+                <PublishButton
+                  isSubmitting={isSubmitting}
+                />
+              </div>
+            )}
           </div>
         </div>
       </div>
       {/* display add ingredients list */}
       <div className="flex flex-col w-full h-full space-y-5">
         <div className="flex items-center justify-between w-full px-2">
-          <h3 className="font-semibold text-lg">List of Ingredients</h3>
+          <h3 className="font-semibold text-base md:text-lg">List of Ingredients</h3>
           <button
             className="flex items-center space-x-1 text-light-gray outline-none pl-3 transition ease-in-out duration-200 hover:underline"
             type="button"
@@ -129,43 +140,63 @@ const PostCard: React.FC<TypeProps> = ({ host, setIsOpen }) => {
             <span className="text-sm">Add Ingredient</span>
           </button>
         </div>
-        <div className="flex flex-col w-full space-y-2">
-         {fields.map((item: any, index: any) => (
-          <div className="flex flex-row items-center w-full px-3 py-2 space-y-1 bg-ghost-white border border-black-matt border-opacity-10 focus-within:border-dark-gray" key={item.id}>
-            <div className="flex flex-col w-full space-y-1">
-              <div className="flex items-center justify-between w-full">
-                <span className="text-xs text-light-gray">Ingredient { index + 1 }</span>
+        <AutoScroll
+          className="flex flex-col w-full h-full max-h-full md:max-h-[23rem] overflow-y-auto space-y-2"
+          showOption={false}
+          scrollBehavior="auto"
+        >
+          <div className="flex flex-col w-full h-full space-y-2">
+            {fields.length === 0 && (
+              <div className="flex flex-col justify-center items-center w-full h-full py-5 md:py-20 space-y-2">
+                <IngredientsIcon />
+                <h1 className="font-normal text-sm text-light-gray">No ingredient at all, add some ingredients...</h1>
               </div>
-              <input
-                className="w-full font-normal text-base outline-none bg-transparent"
-                type="text"
-                {...register(`selected_ingredient.${index}.value`, { required: true })}
-              />
-            </div>
-            <button
-              className="text-light-gray outline-none pl-3 transition ease-in-out duration-200 hover:scale-90"
-              type="button"
-              onClick={() => remove(index)}
-            >
-              <RiDeleteBin4Line className="w-5 h-5" />
-            </button>
+            )}
+            {fields.map((item: any, index: any) => (
+              <div className="flex flex-row items-center w-full px-3 py-2 space-y-1 bg-ghost-white border border-black-matt border-opacity-10 focus-within:border-dark-gray" key={item.id}>
+                <div className="flex flex-col w-full space-y-1">
+                  <div className="flex items-center justify-between w-full">
+                    <span className="text-xs text-light-gray">Ingredient { index + 1 }</span>
+                  </div>
+                  <input
+                    className="w-full font-normal text-base outline-none bg-transparent"
+                    type="text"
+                    {...register(`selected_ingredient.${index}.value`, { required: true })}
+                  />
+                </div>
+                <button
+                  className="text-light-gray outline-none pl-3 transition ease-in-out duration-200 hover:scale-90"
+                  type="button"
+                  onClick={() => remove(index)}
+                >
+                  <RiDeleteBin4Line className="w-5 h-5" />
+                </button>
+              </div>
+            ))}
+            {fields.length !== 0 && (
+              <div className="flex flex-col w-full space-y-2">
+                <button
+                  className="flex justify-center w-full p-4 text-sm border border-black-matt border-opacity-10 hover:border-light-gray transition ease-in-out duration-200 outline-none"
+                  type="button"
+                  onClick={() => append({ value: "" })}
+                >
+                  Add
+                </button>
+                <div className="flex md:hidden w-full">
+                  <PublishButton
+                    isSubmitting={isSubmitting}
+                  />
+                </div>
+                <div className="flex md:hidden w-full">
+                  <h3 className="font-light text-xs text-light-gray">
+                    <span className="font-bold">Note: </span>
+                    <span>{`Don't hit enter if your're not already done inputting your ingredients, because it will published automatically.`}</span>
+                  </h3>
+                </div>
+              </div>
+            )}
           </div>
-         ))}
-         {fields.length === 0 && (
-          <div className="flex justify-center w-full py-3">
-            <h1 className="font-normal text-sm text-light-gray">No ingredients at all.</h1>
-          </div>
-         )}
-         {fields.length !== 0 && (
-          <div className="flex w-full">
-            <PublishButton
-              append={append}
-              isSubmitting={isSubmitting}
-              setIsOpen={setIsOpen}
-            />
-          </div>
-         )}
-        </div>
+        </AutoScroll>
       </div>
     </form>
   )
