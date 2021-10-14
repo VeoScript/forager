fc606d3d6e36feffa140a70818fa665bf4908c09
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-10-14 10:00:29 +0800

    feat: add confirmation modal before deleting a comment (#57)

--- src/components/Home/NewsFeed.tsx
+++ src/components/Home/NewsFeed.tsx
@@ -36,7 +36,7 @@ interface CommentFormTypes {
   dishId: any
 }
 
-interface DeleteComment {
+interface DeleteCommentTypes {
   host: any
   comment: any
 }
@@ -302,25 +302,69 @@ const CommentForm: React.FC<CommentFormTypes> = ({ host, dishId }) => {
 }
 
 // Delete Comment Function (for dynamic form-controls in each posts)
-const DeleteComment: React.FC<DeleteComment> = ({ host, comment }) => {
+const DeleteComment: React.FC<DeleteCommentTypes> = ({ host, comment }) => {
+  const [deleteIsOpen, setDeleteIsOpen] = React.useState(false)
   const userId = host.id
   const commentId = comment.id
   return (
-    <button
-      className="transition ease-in-out duration-200 transform hover:scale-95"
-      type="button"
-      onClick={async () => {
-        await fetch('/api/comments/delete', {
-          method: 'DELETE',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ userId, commentId })
-        })
-      }}
-    >
-      <RiChatDeleteLine />
-    </button>
+    <React.Fragment>
+      <button
+        className="transition ease-in-out duration-200 transform hover:scale-95"
+        type="button"
+        onClick={() => setDeleteIsOpen(true)}
+      >
+        <RiChatDeleteLine className="text-black text-opacity-30" />
+      </button>
+      {deleteIsOpen && (
+        <React.Fragment>
+          <button 
+            className={`${deleteIsOpen ? 'z-10 block fixed inset-0 w-full h-full bg-black-matt bg-opacity-50 cursor-default focus:outline-none' : 'hidden'}`}
+            type="button"
+            onClick={() => {
+              setDeleteIsOpen(false)
+            }} 
+          />
+          <div className="absolute inset-0 flex flex-col items-center justify-center w-full">
+            <div className="flex flex-row items-center justify-center align-middle z-10 w-full max-w-sm shadow-sm bg-pure-white border border-black-matt border-opacity-10">
+              <div className="flex flex-col w-full p-3">
+                <div className="flex w-full px-3 py-2">
+                  <span className="font-bold text-sm">Delete Comment</span>
+                </div>
+                <div className="flex w-full px-3 py-1">
+                  <span className="font-normal text-xs">
+                    Delete your comment permanently?
+                  </span>
+                </div>
+                <div className="flex items-center justify-end w-full space-x-1 px-3 py-2">
+                  <button
+                    className="flex px-3 py-2 text-xs border border-black-matt border-opacity-10 hover:border-light-gray outline-none"
+                    type="button"
+                    onClick={() => setDeleteIsOpen(false)}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    className="flex px-3 py-2 text-xs border border-black-matt border-opacity-10 hover:border-red-500 hover:text-red-500 outline-none"
+                    type="button"
+                    onClick={async () => {
+                      await fetch('/api/comments/delete', {
+                        method: 'DELETE',
+                        headers: {
+                          'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({ userId, commentId })
+                      })
+                    }}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </React.Fragment>
+      )}
+    </React.Fragment>
   )
 }
 
