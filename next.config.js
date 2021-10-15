54c75a2c40121c32d814d3d28c145999de5182d5
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-10-15 10:24:35 +0800

    fix: authentication api-route security issue (#64)
    * feat: add auth_secret for the users credentials security
    
    * chore: add auth_secret in env global
    
    * refactor: change users to auth_secret

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  env: {
+    AUTH_SECRET: process.env.AUTH_SECRET,
+  }
 }
--- src/pages/api/auth/[auth_secret].ts
+++ src/pages/api/auth/[auth_secret].ts
@@ -2,6 +2,15 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '~/lib/Prisma'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { auth_secret } = req.query
+
+  if (auth_secret !== process.env.AUTH_SECRET) {
+    res.writeHead(301, {
+      Location: "/signin",
+    })
+    res.end()
+  }
+
   if (req.method !== 'GET') {
     res.status(500).json('GET Method Only')
   } else {
